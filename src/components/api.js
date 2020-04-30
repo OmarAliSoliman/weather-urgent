b2a0c27d765d6cd9e4f4e844ff265e11981edd1f
omar <49602133+OmarAliSoliman@users.noreply.github.com>
2020-05-01 01:54:56 +0200

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 const Api_Key= "42ebc09f50dbc6c2cdf68e75e607e38a"
 
 export async function getdate(city) {
-  const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},&APPID=${Api_Key}`);
+  const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},&APPID=${Api_Key}`);
   return response;
 }
